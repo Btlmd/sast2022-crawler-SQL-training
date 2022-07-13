da36e4269418fea1b26da412c8f46f544116ad3a
LambdaX <joshua@btlmd.com>
2022-07-13 14:46:12 +0800

    initial commit

---
+++ WebVPN_crawler/webvpn.py
@@ -0,0 +1,144 @@
+from selenium.webdriver.remote.webdriver import WebDriver as wd
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait as wdw
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains as AC
+import selenium
+from bs4 import BeautifulSoup as BS
+
+class WebVPN:
+    def __init__(self, opt: dict, headless=False):
+        self.root_handle = None
+        self.driver: wd = None
+        self.passwd = opt["passwd"]
+        self.userid = opt["id"]
+        self.headless = headless
+
+    def login_webvpn(self):
+        """
+        Log in to WebVPN with the account specified in `self.userid` and `self.passwd`
+
+        :return:
+        """
+        d = self.driver
+        if d is not None:
+            d.close()
+        d = selenium.webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        d.get("https://webvpn.tsinghua.edu.cn/login")
+        username = d.find_elements(By.XPATH,
+                                   '//div[@class="login-form-item"]//input'
+                                   )[0]
+        password = d.find_elements(By.XPATH,
+                                   '//div[@class="login-form-item password-field" and not(@id="captcha-wrap")]//input'
+                                   )[0]
+        username.send_keys(str(self.userid))
+        password.send_keys(self.passwd)
+        d.find_element(By.ID, "login").click()
+        self.root_handle = d.current_window_handle
+        self.driver = d
+        return d
+
+    def access(self, url_input):
+        """
+        Jump to the target URL in WebVPN
+
+        :param url_input: target URL
+        :return:
+        """
+        d = self.driver
+        url = By.ID, "quick-access-input"
+        btn = By.ID, "go"
+        wdw(d, 5).until(EC.visibility_of_element_located(url))
+        actions = AC(d)
+        actions.move_to_element(d.find_element(*url))
+        actions.click()
+        actions.\
+            key_down(Keys.CONTROL).\
+            send_keys("A").\
+            key_up(Keys.CONTROL).\
+            send_keys(Keys.DELETE).\
+            perform()
+
+        d.find_element(*url)
+        d.find_element(*url).send_keys(url_input)
+        d.find_element(*btn).click()
+
+    def switch_another(self):
+        """
+        If there are only 2 windows handles, switch to the other one
+
+        :return:
+        """
+        d = self.driver
+        assert len(d.window_handles) == 2
+        wdw(d, 5).until(EC.number_of_windows_to_be(2))
+        for window_handle in d.window_handles:
+            if window_handle != d.current_window_handle:
+                d.switch_to.window(window_handle)
+                return
+
+    def to_root(self):
+        """
+        Switch to the home page of WebVPN
+
+        :return:
+        """
+        self.driver.switch_to.window(self.root_handle)
+
+    def close_all(self):
+        """
+        Close all window handles
+
+        :return:
+        """
+        while True:
+            try:
+                l = len(self.driver.window_handles)
+                if l == 0:
+                    break
+            except selenium.common.exceptions.InvalidSessionIdException:
+                return
+            self.driver.switch_to.window(self.driver.window_handles[0])
+            self.driver.close()
+
+    def login_info(self):
+        """
+        TODO: After successfully logged into WebVPN, login to info.tsinghua.edu.cn
+
+        :return:
+        """
+
+        # Hint: - Wait until the elements are ready
+        #       - Before return, make sure that you have logged in successfully
+        raise NotImplementedError
+
+    def get_grades(self):
+        """
+        TODO: Get and calculate the GPA for each semester.
+
+        Example return / print:
+            2020-秋: *.**
+            2021-春: *.**
+            2021-夏: *.**
+            2021-秋: *.**
+            2022-春: *.**
+
+        :return:
+        """
+
+        # Hint: - You can directly switch into
+        #         `zhjw.cic.tsinghua.edu.cn/cj.cjCjbAll.do?m=bks_cjdcx&cjdlx=zw`
+        #         after logged in
+        #       - You can use Beautiful Soup to parse the HTML content or use
+        #         XPath directly to get the contents
+        #       - You can use `element.get_attribute("innerHTML")` to get its
+        #         HTML code
+
+        raise NotImplementedError
+
+if __name__ == "__main__":
+    # TODO: Write your own query process
+    raise NotImplementedError
