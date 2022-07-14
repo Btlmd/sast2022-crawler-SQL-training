4adf8a60d016c46b320d1010a541bdd74d78d23f
LambdaX <joshua@btlmd.com>
2022-07-15 07:02:48 +0800

    purged

---
+++ README.md
@@ -0,0 +1,234 @@
+> 爬虫与数据库 课后练习
+>
+> Lambda X
+>
+> 2022 年 7 月 13 日
+
+# requests 练习
+
+本次爬虫练习中，希望你动手实现一个知乎热榜定时跟踪器。使得他可以定期爬取知乎热榜，并记录热榜中问题的一些基本信息，如问题摘要、描述、热度、访问人数、回答数量等等，然后将这些数据存入数据库进行保存。
+
+Choice 1: 删除 `Zhihu_crawler/zhihu.py` 然后从零开始写 :feet:
+
+Choice 2: 阅读 `Zhihu_crawler/zhihu.py`，完成约 40 行的代码填空 :happy: 
+
+## 代码填空介绍
+
+代码填空中已经实现好爬虫的主要逻辑，并实现了数据表的创建与修改语句。但它并不能获取任何数据。这是因为它获取热榜的 `get_board` 方法和 `get_question` 方法尚未实现。
+
+请你按照提示实现这两个方法，给出相应的返回值。然后就可以让它定时跟踪知乎热榜并将内容存入数据库了。
+
+### 推荐实现顺序
+
+1. 在 `Zhihu_crawler/zhihu.json` 中填入
+    - 你认为可能会用到的 Headers
+    - MySQL 服务器的配置信息
+
+    此时运行结果为
+    ```bash
+    2022-07-09 18:19:54.295 [INFO] Settings loaded
+    2022-07-09 18:19:54.419 [INFO] Begin crawling ...
+    2022-07-09 18:19:54.539 [ERROR] Crawl 161 encountered an exception . This crawl stopped.
+    Traceback (most recent call last):
+      File "zhihu.py", line 96, in watch
+        board_entries = self.get_board()
+      File "zhihu.py", line 259, in get_board
+        raise NotImplementedError
+    NotImplementedError
+    2022-07-09 18:19:54.543 [INFO] Sleep 599.8760051727295 second(s)
+    ```
+    提示你应该实现 `get_board`。
+
+2. 在 `Zhihu_crawler/zhihu.py` 中按照提示实现 `get_board` 方法。再运行 `zhihu.py` 此时运行结果类似
+   ```bash
+   2022-07-09 18:20:38.343 [INFO] Settings loaded
+    2022-07-09 18:20:38.496 [INFO] Begin crawling ...
+    2022-07-09 18:20:39.534 [INFO] Get 50 items: 安倍晋三遭枪击身亡，按照日本法律行凶者涉,如何看待周杰伦新专辑全部曲目中只有 7 ,化学键断裂要耗能，为什么生物教材上写 A,如何看待报告...
+    2022-07-09 18:20:39.535 [INFO] Sleep 2 second(s)
+    2022-07-09 18:20:41.549 [ERROR] 
+    Traceback (most recent call last):
+      File "zhihu.py", line 122, in watch
+        detail = self.get_question(item["qid"])
+      File "zhihu.py", line 307, in get_question
+        raise NotImplementedError
+    NotImplementedError
+    2022-07-09 18:20:41.677 [INFO] Sleep 2 second(s)
+   ```
+   此时爬虫已经可以正常获取热榜（`watch` 方法中，会将热榜中每个问题截取 20 个字符进行显示），但还无法获取问题页面中的信息。
+
+   在代码中有一些实现提示。
+
+3. 在 `Zhihu_crawler/zhihu.py` 中按照提示实现 `get_question` 方法。再运行 `zhihu.py` 此时运行结果类似
+   ```bash
+   2022-07-09 18:26:15.442 [INFO] Settings loaded
+   2022-07-09 18:26:15.737 [INFO] Begin crawling ...
+   2022-07-09 18:26:16.760 [INFO] Get 50 items: 安倍晋三遭枪击身亡，按照日本法律行凶者涉,如何看待周杰伦新专辑全部曲目中只有 7 ,化学键断裂要耗能，为什么生物教材上写 A,如何看待报告...
+   2022-07-09 18:26:16.761 [INFO] Sleep 2 second(s)
+   2022-07-09 18:26:19.652 [INFO] Get question detail for 安倍晋三遭枪击身亡，按照日本法律行凶者涉嫌何种罪名？会不会被判处死刑？: raw detail length 498
+   2022-07-09 18:26:19.780 [INFO] Sleep 2 second(s)
+   2022-07-09 18:26:22.709 [INFO] Get question detail for 如何看待周杰伦新专辑全部曲目中只有 7 首新歌，收录了之前的 5 首老歌？: raw detail length 663
+   2022-07-09 18:26:22.834 [INFO] Sleep 2 second(s)
+   2022-07-09 18:26:25.708 [INFO] Get question detail for 化学键断裂要耗能，为什么生物教材上写 ATP 高能磷酸键断裂会释放大量能量？: raw detail length 0
+   2022-07-09 18:26:25.832 [INFO] Sleep 2 second(s)
+   ```
+
+   在代码中有一些实现提示。
+
+4. 实现过程中，建议另开 `Jupyter Notebook` 进行调试。~~甚至可以调好了再把代码复制进去。~~如果需要，可以对给出的代码任意修改。
+
+5. 添加其他好玩的功能
+
+
+
+# Selenium 练习
+
+在本次 `Selenium `练习中，希望你动手实现一个 GPA 计算器。
+
+一种参考实现是：使用 `Selenium `模拟点击登录 WebVPN，然后登录 info，进而访问成绩单页面查询到成绩，计算每学期的绩点。
+
+使用 `Selenium` 进行 WebVPN 登录并跳转到校内的指定页面的代码码见 `WebVPN/webvpn.py`。 你需要设计并实现查询过程中可能用到的函数，并补全查询流程。这部分内容在代码中用 `TODO` 标出。当然也可以完全不理会这些东西，自己进行实现。
+
+成绩单的页面的信息编码在了 `HTML` 中。你可能需要使用 `BeautifulSoup` 或 `lxml` 等工具进行解析。
+
+
+
+# SQL 练习
+
+SQL 练习希望能帮助你熟悉一些基本的 SQL 语法。
+
+我们同样以知乎爬虫数据为背景，在 MySQL 数据库中进行练习。先下载导入 7 月 8 日至 7 月 12 日的一部分知乎热榜数据。
+
+**注意：不要和你的爬虫共用一个数据库，因为导入的过程中会删除已经存在的数据。**
+
+```bash
+curl -fsSL https://cloud.tsinghua.edu.cn/f/a46b322b89064c03b89e/?dl=1 -o exercise.sql.gz 
+gzip -dc exercise.sql.gz > exercise.sql
+```
+
+然后进入 MySQL，在新建并使用数据库；可以用 `source` 执行 `exercise.sql` ，将预置的数据导入。
+
+```bash
+mysql -u <username> -p
+
+mysql> create database s2;
+Query OK, 1 row affected (0.02 sec)
+
+mysql> use s2;
+Database changed
+
+mysql> source exercise.sql;
+
+mysql> show tables;
++--------------+
+| Tables_in_s2 |
++--------------+
+| crawl        |
+| record       |
++--------------+
+2 rows in set (0.00 sec)
+```
+
+这样就成功导入了表。
+
+导入数据的字段类型与含义与爬虫部分的设计一致（事实上它们就是我测试爬虫框架时产生的数据），可以在 `zhihu.py` 的 `create_tables` 函数中查看字段设计及含义。
+
+每个问题的提示是一些**可能**对完成这次查询有帮助的 MySQL 关键字；在`SQL_query/example.log` 给出了每个问题的参考查询结果，但将查询语句用 `<Q*>` 隐去，便于你参考自己的查询实现是否正确。
+
+
+
+## Q1 添加新列
+>  给 `record` 表添加一列 `heat_w`，准备以万为单位为储存 INT 型的热度。
+- 提示：
+  - ALTER TABLE
+  - 使用 `DESC record;` 查看表中的所有列。
+  - 使用 `SHOW COLUMNS FROM record LIKE 'heat%';` 查看表中以 `heat` 开头的列。
+## Q2 快速填充
+
+> 给 `heat_w` 一列以**万**为单位填入热度。
+>
+> 如 ` 114514万热度` 时填入 `114514`
+- 提示：
+  - UPDATE
+  - REPLACE
+- 可以使用
+  ```mysql
+  SELECT COUNT(*) cnt FROM record WHERE heat_w is null and heat is not null;
+  ```
+  检查还有多少列没有填好（即原来非空但 heat_w 为空）
+
+## Q3 查询关键词
+
+> 查询标题中包含 '高考' 的问题标题。每个问题标题仅显示一次
+
+- 提示
+  - SELECT 
+  - DISTINCT
+  - WHERE 
+  - LIKE
+
+
+## Q4 问题走势
+
+> 查询问题 `507238700` 的 `heat`, `hit_at`, `visitCount`, `followerCount` ，从旧到新
+
+- 提示：
+  - SELECT
+  - WHERE
+  - ORDER BY
+
+注1：问题是随便选的
+
+注2：有兴趣的同学可以在 Python 中执行这一查询，然后使用 `matplotlib` 画出相应指标随时间变化的走势图。
+
+## Q5 上榜统计
+
+> 查询每个 `title` 上榜的次数，只返回上榜 100 次以上的 `title` 和 `上榜次数`。由多到少降序排列。
+- 提示 
+  - SELECT
+  - COUNT
+  - GROUP BY
+  - HAVING
+  - ORDER BY
+
+
+## Q6 最热记录
+
+> 查询浏览次数最多的问题，获取它浏览次数最多的那一次记录中的 `title`, `visitCount`, `hit_at`, `heat`, `ranking` 。
+- 提示
+  - SELECT
+  - WHERE + Sub Query
+  - MAX
+
+## Q7 查阅合订本
+> 查询哪些问题的**标题至少变化了一次**。对于这些问题，查询其 `qid` 和 `title` ，每个不同的标题仅返回 1 次。每个问题的不同标题连续显示，按 `qid` 升序排列。
+
+- 提示：
+  - SELECT
+  - JOIN + Sub Query
+  - COUNT
+  - DISTINCT
+  - ORDER BY
+
+
+
+# 提交作业
+
+通过 clone / fork 代码仓库 **[sast2022-crawler-SQL-training](https://github.com/Btlmd/sast2022-crawler-SQL-training)** 完成作业
+
+- requests 练习，提交在 `Zhihu_crawler` 目录下
+  - 代码
+  - README: 简要说明使用方法
+  - 爬到的一些数据，导出至少爬 5 次热榜（如果觉得 10 分钟间隔不合适可以自行调整）的数据表，导出为 `sql` 文件。导出后文件较大的，可以使用 gzip 压缩或者上传云盘附上链接。
+  - 导出方法见课前准备材料。
+- Selenium 练习，提交在 `WebVPN_crawler` 目录下
+  - 代码
+  - README: 简要说明使用方法
+- SQL 练习，提交在 `SQL_query` 目录下
+  - 请在练习数据库中使用 `tee` 指令输出查询日志，然后提交上述 7 条查询产生的查询日志文件
+- 其他你希望包括的内容
+
+完成作业后，将代码进行托管，如 Github， Tsinghua Git 等，然后在原仓库中新建 Issue，提交代码仓库地址。
+
+可以在 Issue 中附上学号或邮箱，**或** 将 Issue 编号与学号发送至 [liu-md20@mails.tsinghua.edu.cn](mailto:liu-md20@mails.tsinghua.edu.cn) ，便于我们统计大家的参与情况并进行作业奖励的发放。
+
+为了最佳的练习体验，希望您能尽早完成练习。:)
---
+++ Zhihu_crawler/zhihu.py
@@ -0,0 +1,312 @@
+﻿import requests
+import json
+import pymysql
+from bs4 import BeautifulSoup as BS
+import logging
+import time
+
+fmt = '%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s'
+datefmt = '%Y-%m-%d %H:%M:%S'
+level = logging.INFO
+
+formatter = logging.Formatter(fmt, datefmt)
+logger = logging.getLogger()
+logger.setLevel(level)
+
+file = logging.FileHandler("../zhihu.log", encoding='utf-8')
+file.setLevel(level)
+file.setFormatter(formatter)
+logger.addHandler(file)
+
+console = logging.StreamHandler()
+console.setLevel(level)
+console.setFormatter(formatter)
+logger.addHandler(console)
+
+
+class ZhihuCrawler:
+    def __init__(self):
+        with open("zhihu.json", "r", encoding="utf8") as f:
+            self.settings = json.load(f)  # Load settings
+        logger.info("Settings loaded")
+
+
+    def sleep(self, sleep_key, delta=0):
+        """
+        Execute sleeping for a time configured in the settings
+
+        :param sleep_key: the sleep time label
+        :param delta: added to the sleep time
+        :return:
+        """
+        _t = self.settings["config"][sleep_key] + delta
+        logger.info(f"Sleep {_t} second(s)")
+        time.sleep(_t)
+
+    def query(self, sql, args=None, op=None):
+        """
+        Execute an SQL query
+
+        :param sql: the SQL query to execute
+        :param args: the arguments in the query
+        :param op: the operation to cursor after query
+        :return: op(cur)
+        """
+        conn = pymysql.connect(
+            cursorclass=pymysql.cursors.DictCursor,
+            client_flag=pymysql.constants.CLIENT.MULTI_STATEMENTS,
+            **self.settings['mysql']
+        )
+        if args and not (isinstance(args, tuple) or isinstance(args, list)):
+            args = (args,)
+        with conn:
+            with conn.cursor() as cur:
+                try:
+                    cur.execute(sql, args)
+                    conn.commit()
+                    if op is not None:
+                        return op(cur)
+                except:  # Log query then exit
+                    if hasattr(cur, "_last_executed"):
+                        logger.error("Exception @ " + cur._last_executed)
+                    else:
+                        logger.error("Exception @ " + sql)
+                    raise
+
+    def watch(self, top=None):
+        """
+        The crawling flow
+
+        :param top: only look at the first `top` entries in the board. It can be used when debugging
+        :return:
+        """
+        self.create_table()
+        while True:
+            logger.info("Begin crawling ...")
+            try:
+                crawl_id = None
+                begin_time = time.time()
+                crawl_id = self.begin_crawl(begin_time)
+
+                try:
+                    board_entries = self.get_board()
+                except RuntimeError as e:
+                    if isinstance(e.args[0], requests.Response):
+                        logger.exception(e.args[0].status_code, e.args[0].text)
+                    raise
+                else:
+                    logger.info(
+                        f"Get {len(board_entries)} items: {','.join(map(lambda x: x['title'][:20], board_entries))}")
+                if top:
+                    board_entries = board_entries[:top]
+
+                # Process each entry in the hot list
+                for idx, item in enumerate(board_entries):
+                    self.sleep("interval_between_question")
+                    detail = {
+                        "created": None,
+                        "visitCount": None,
+                        "followerCount": None,
+                        "answerCount": None,
+                        "raw": None,
+                        "hit_at": None
+                    }
+                    if item["qid"] is None:
+                        logger.warning(f"Unparsed URL @ {item['url']} ranking {idx} in crawl {crawl_id}.")
+                    else:
+                        try:
+                            detail = self.get_question(item["qid"])
+                        except Exception as e:
+                            if len(e.args) > 0 and isinstance(e.args[0], requests.Response):
+                                logger.exception(f"{e}; {e.args[0].status_code}; {e.args[0].text}")
+                            else:
+                                logger.exception(f"{str(e)}")
+                        else:
+                            logger.info(f"Get question detail for {item['title']}: raw detail length {len(detail['raw']) if detail['raw'] else 0}")
+                    try:
+                        self.add_entry(crawl_id, idx, item, detail)
+                    except Exception as e:
+                        logger.exception(f"Exception when adding entry {e}")
+                self.end_crawl(crawl_id)
+            except Exception as e:
+                logger.exception(f"Crawl {crawl_id} encountered an exception {e}. This crawl stopped.")
+            self.sleep("interval_between_board", delta=(begin_time - time.time()))
+
+    def create_table(self):
+        """
+        Create tables to store the hot question records and crawl records
+
+        """
+        sql = f"""
+CREATE TABLE IF NOT EXISTS `crawl` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `begin` DOUBLE NOT NULL,
+    `end` DOUBLE,
+    PRIMARY KEY (`id`) USING BTREE
+)
+AUTO_INCREMENT = 1 
+CHARACTER SET = utf8mb4 
+COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `record`  (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `qid` INT NOT NULL,
+    `crawl_id` BIGINT NOT NULL,
+    `hit_at` DOUBLE,
+    `ranking` INT NOT NULL,
+    `title` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL ,
+    `heat` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created` INT,
+    `visitCount` INT,
+    `followerCount` INT,
+    `answerCount` INT,
+    `excerpt` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    `raw` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ,
+    `url` VARCHAR(255),
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `CrawlAssociation` (`crawl_id`) USING BTREE,
+    CONSTRAINT `CrawlAssociationFK` FOREIGN KEY (`crawl_id`) REFERENCES `crawl` (`id`)
+) 
+AUTO_INCREMENT = 1 
+CHARACTER SET = utf8mb4 
+COLLATE = utf8mb4_unicode_ci;
+
+"""
+        self.query(sql)
+
+    def begin_crawl(self, begin_time) -> (int, float):
+        """
+        Mark the beginning of a crawl
+        :param begin_time:
+        :return: (Crawl ID, the time marked when crawl begin)
+        """
+        sql = """
+INSERT INTO crawl (begin) VALUES(%s);
+"""
+        return self.query(sql, begin_time, lambda x: x.lastrowid)
+
+    def end_crawl(self, crawl_id: int):
+        """
+        Mark the ending time of a crawl
+
+        :param crawl_id: Crawl ID
+        """
+        sql = """
+UPDATE crawl SET end = %s WHERE id = %s;
+"""
+        self.query(sql, (time.time(), crawl_id))
+
+    def add_entry(self, crawl_id, idx, board, detail):
+        """
+        Add a question entry to database
+
+        :param crawl_id: Crawl ID
+        :param idx: Ranking in the board
+        :param board: dict, info from the board
+        :param detail: dict, info from the detail page
+        """
+        sql = \
+            """
+INSERT INTO record (`qid`, `crawl_id`, `title`, `heat`, `created`, `visitCount`, `followerCount`, `answerCount`,`excerpt`, `raw`, `ranking`, `hit_at`, `url`)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+"""
+        self.query(
+            sql,
+            (
+                board["qid"],
+                crawl_id,
+                board["title"],
+                board["heat"],
+                detail["created"],
+                detail["visitCount"],
+                detail["followerCount"],
+                detail["answerCount"],
+                board["excerpt"],
+                detail["raw"],
+                idx,
+                detail["hit_at"],
+                board["url"]
+            )
+        )
+
+    def get_board(self) -> list:
+        """
+        TODO: Fetch current hot questions
+
+        :return: hot question list, ranking from high to low
+
+        Return Example:
+        [
+            {
+                'title': '针对近期生猪市场非理性行为，国家发展改革委研究投放猪肉储备，此举对市场将产生哪些积极影响？',
+                'heat': '76万热度',
+                'excerpt': '据国家发展改革委微信公众号 7 月 5 日消息，针对近期生猪市场出现盲目压栏惜售等非理性行为，国家发展改革委价格司正研究启动投放中央猪肉储备，并指导地方适时联动投放储备，形成调控合力，防范生猪价格过快上涨。',
+                'url': 'https://www.zhihu.com/question/541600869',
+                'qid': 541600869,
+            },
+            {
+                'title': '有哪些描写夏天的古诗词？',
+                'heat': '41万热度',
+                'excerpt': None,
+                'url': 'https://www.zhihu.com/question/541032225',
+                'qid': 541032225,
+            },
+            {
+                'title':    # 问题标题
+                'heat':     # 问题热度
+                'excerpt':  # 问题摘要
+                'url':      # 问题网址
+                'qid':      # 问题编号
+            }
+            ...
+        ]
+        """
+
+        # Hint: - Parse HTML, pay attention to the <section> tag.
+        #       - Use keyword argument `class_` to specify the class of a tag in `find`
+        #       - Hot Question List can be accessed in https://www.zhihu.com/hot
+
+        raise NotImplementedError
+
+    def get_question(self, qid: int) -> dict:
+        """
+        TODO: Fetch question info by question ID
+
+        :param qid: Question ID
+        :return: a dict of question info
+
+        Return Example:
+        {
+            "created": 1657248657,      # 问题的创建时间
+            "followerCount": 5980,      # 问题的关注数量
+            "visitCount": 2139067,      # 问题的浏览次数
+            "answerCount": 2512         # 问题的回答数量
+            "title": "日本前首相安倍      # 问题的标题
+                晋三胸部中枪已无生命
+                体征 ，嫌疑人被控制，
+                目前最新进展如何？背
+                后原因为何？",
+            "raw": "<p>据央视新闻，        # 问题的详细描述
+                当地时间8日，日本前
+                首相安倍晋三当天上午
+                在奈良发表演讲时中枪
+                。据悉，安倍晋三在上
+                救护车时还有意。。。",
+            "hit_at": 1657264954.3134503  # 请求的时间戳
+        }
+
+
+        """
+
+        # Hint: - Parse JSON, which is embedded in a <script> and contains all information you need.
+        #       - After find the element in soup, use `.text` attribute to get the inner text
+        #       - Use `json.loads` to convert JSON string to `dict` or `list`
+        #       - You may first save the JSON in a file, format it and locate the info you need
+        #       - Use `time.time()` to create the time stamp
+        #       - Question can be accessed in https://www.zhihu.com/question/<Question ID>
+
+        raise NotImplementedError
+
+if __name__ == "__main__":
+    z = ZhihuCrawler()
+    z.watch()
