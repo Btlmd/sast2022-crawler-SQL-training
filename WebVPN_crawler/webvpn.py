686bc3e2af90e6a21f5ab60d92b8f5d2a8723ad9
Lambda X <joshua@btlmd.com>
2022-07-19 06:49:41 +0800

    WebVPN Impl

--- WebVPN_crawler/webvpn.py
+++ WebVPN_crawler/webvpn.py
@@ -8,21 +8,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains as AC
 import selenium
 from bs4 import BeautifulSoup as BS
+import json
 
 class WebVPN:
-    def __init__(self, opt: dict, headless=False):
+    def __init__(self, opt, headless=False):
         self.root_handle = None
         self.driver: wd = None
-        self.passwd = opt["username"]
-        self.userid = opt["password"]
+        self.passwd = opt["password"]
+        self.userid = opt["username"]
         self.headless = headless
 
     def login_webvpn(self):
-        """
-        Log in to WebVPN with the account specified in `self.userid` and `self.passwd`
-
-        :return:
-        """
         d = self.driver
         if d is not None:
             d.close()
@@ -42,12 +38,6 @@ class WebVPN:
         return d
 
     def access(self, url_input):
-        """
-        Jump to the target URL in WebVPN
-
-        :param url_input: target URL
-        :return:
-        """
         d = self.driver
         url = By.ID, "quick-access-input"
         btn = By.ID, "go"
@@ -55,23 +45,13 @@ class WebVPN:
         actions = AC(d)
         actions.move_to_element(d.find_element(*url))
         actions.click()
-        actions.\
-            key_down(Keys.CONTROL).\
-            send_keys("A").\
-            key_up(Keys.CONTROL).\
-            send_keys(Keys.DELETE).\
-            perform()
+        actions.key_down(Keys.CONTROL).send_keys("A").key_up(Keys.CONTROL).send_keys(Keys.DELETE).perform()
 
         d.find_element(*url)
         d.find_element(*url).send_keys(url_input)
         d.find_element(*btn).click()
 
     def switch_another(self):
-        """
-        If there are only 2 windows handles, switch to the other one
-
-        :return:
-        """
         d = self.driver
         assert len(d.window_handles) == 2
         wdw(d, 5).until(EC.number_of_windows_to_be(2))
@@ -81,19 +61,9 @@ class WebVPN:
                 return
 
     def to_root(self):
-        """
-        Switch to the home page of WebVPN
-
-        :return:
-        """
         self.driver.switch_to.window(self.root_handle)
 
     def close_all(self):
-        """
-        Close all window handles
-
-        :return:
-        """
         while True:
             try:
                 l = len(self.driver.window_handles)
@@ -105,42 +75,69 @@ class WebVPN:
             self.driver.close()
 
     def login_info(self):
-        """
-        TODO: After successfully logged into WebVPN, login to info.tsinghua.edu.cn
-
-        :return:
-        """
-
-        # Hint: - Use `access` method to jump to info.tsinghua.edu.cn
-        #       - Use `switch_another` method to change the window handle
-        #       - Wait until the elements are ready, then preform your actions
-        #       - Before return, make sure that you have logged in successfully
-        raise NotImplementedError
+        d = self.driver
+        username = By.ID, "userName"
+        password = By.NAME, "password"
+        wdw(d, 10).until(EC.visibility_of_element_located(username))
+        d.find_element(*username).send_keys(str(self.userid))
+        d.find_element(*password).send_keys(self.passwd)
+        d.find_element(By.XPATH, '//td/input[@type="image"]').click()
+        wdw(d, 15).until(EC.visibility_of_element_located((By.XPATH, '//span[text()="信息门户"]')))
 
     def get_grades(self):
-        """
-        TODO: Get and calculate the GPA for each semester.
+        wdw(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'table1')))
+        html = self.driver.find_elements(By.TAG_NAME, "HTML")[0].get_attribute("innerHTML")
+        soup = BS(html, 'lxml')
+        grades = []
+        table = soup.select("#table1 tbody")[0].find_all('tr')
+        # print(table)
+        for tr in table:
+            tds = list(tr.find_all('td'))
+            if len(tds) == 0:
+                continue
+            grades += [{
+                "id": tds[0].text.strip(),
+                "name": tds[1].text.strip(),
+                "credit": tds[2].text.strip(),
+                "grade": tds[3].text.strip(),
+                "points": tds[4].text.strip(),
+                "term": tds[5].text.strip(),
+            }]
+        terms = {}
+        pv = None
+        for course in grades:
+            if course["term"] != pv:
+                terms[course["term"]] = []
+                pv = course["term"]
+            terms[course["term"]] += [course]
 
-        Example return / print:
-            2020-秋: *.**
-            2021-春: *.**
-            2021-夏: *.**
-            2021-秋: *.**
-            2022-春: *.**
+        term_gpa = {}
+        for k, courses in terms.items():
+            deno = 0
+            summation = 0
+            for c in courses:
+                if c["points"] == "N/A":
+                    continue
+                summation += float(c["points"]) * int(c["credit"])
+                deno += int(c["credit"])
+            gpa = summation / deno
+            term_gpa[k] = gpa
 
-        :return:
-        """
-
-        # Hint: - You can directly switch into
-        #         `zhjw.cic.tsinghua.edu.cn/cj.cjCjbAll.do?m=bks_cjdcx&cjdlx=zw`
-        #         after logged in
-        #       - You can use Beautiful Soup to parse the HTML content or use
-        #         XPath directly to get the contents
-        #       - You can use `element.get_attribute("innerHTML")` to get its
-        #         HTML code
-
-        raise NotImplementedError
+        for k, v in term_gpa.items():
+            print(f"{k}: {round(v, 2)}")
 
 if __name__ == "__main__":
-    # TODO: Write your own query process
-    raise NotImplementedError
+    with open("settings.json", "r") as f:
+        opt = json.load(f)
+
+    v = WebVPN(opt)
+    v.login_webvpn()
+    v.access("info.tsinghua.edu.cn")
+    v.switch_another()
+    v.login_info()
+    v.driver.close()
+    v.to_root()
+    v.access("zhjw.cic.tsinghua.edu.cn/cj.cjCjbAll.do?m=bks_cjdcx&cjdlx=zw")
+    v.switch_another()
+    v.get_grades()
+    v.close_all()
